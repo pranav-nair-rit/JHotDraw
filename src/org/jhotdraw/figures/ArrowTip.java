39684b3bc6e953976781c6355c44ff0fdb7e1d35
pranav-nair-rit <pn1922@g.rit.edu>
2022-04-01 21:15:31 -0400

    2 commit for solving violation 'Call Super In Constructor'. I have called the super constructor in the code

--- src/org/jhotdraw/figures/ArrowTip.java
+++ src/org/jhotdraw/figures/ArrowTip.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	ï¿½ by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
@@ -43,6 +43,7 @@ public  class ArrowTip extends AbstractLineDecoration {
 	* Constructs an arrow tip with the given angle and radius.
 	*/
 	public ArrowTip(double angle, double outerRadius, double innerRadius) {
+		super();
 		setAngle(angle);
 		setOuterRadius(outerRadius);
 		setInnerRadius(innerRadius);
